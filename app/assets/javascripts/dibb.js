590284d3c29b8ac24ae4325935bc886e1404cf30
nicklaiacona <nick@performantsoftware.com>
2015-12-08 09:57:56 -0500

    don't load dibb spa unless logged in

--- app/assets/javascripts/dibb.js
+++ app/assets/javascripts/dibb.js
@@ -12,4 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require underscore
+//= require backbone
 //= require bootstrap-sprockets
+//= require_tree ./lib
+//= require_tree ./dibb
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    render layout: "dibb"
   end
 
 
