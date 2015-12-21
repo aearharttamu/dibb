0e2c8b5824f04419684de85ad7bd8eee14aeac8f
nicklaiacona <nick@performantsoftware.com>
2015-12-21 15:19:06 -0500

    uncomment neography init

--- config/initializers/neography.rb
+++ config/initializers/neography.rb
@@ -1,8 +1,8 @@
-# # these are the default values:
-# Neography.configure do |config|
-#   config.protocol             = Rails.application.secrets.neo4j['protocol']
-#   config.server               = Rails.application.secrets.neo4j['server']
-#   config.port                 = Rails.application.secrets.neo4j['port']
-#   config.username             = Rails.application.secrets.neo4j['username']
-#   config.password             = Rails.application.secrets.neo4j['password']
-# end
+# these are the default values:
+Neography.configure do |config|
+  config.protocol             = Rails.application.secrets.neo4j['protocol']
+  config.server               = Rails.application.secrets.neo4j['server']
+  config.port                 = Rails.application.secrets.neo4j['port']
+  config.username             = Rails.application.secrets.neo4j['username']
+  config.password             = Rails.application.secrets.neo4j['password']
+end
