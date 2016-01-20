22eb0ea0845a5a3b440e05faa4849d6fdf45ca14
nicklaiacona <nick@performantsoftware.com>
2016-01-20 09:44:23 -0500

    refactor where genre model is held

---
+++ app/assets/javascripts/dibb/models/genres.js
@@ -0,0 +1,9 @@
+DiBB.Genres = [  
+	{ text: "Print Monograph", value: "print-monograph", type: "single-item"},
+	{ text: "Digital Monograph", value: "digital-monograph", type: "single-item"},
+	{ text: "Print Serial", value: "print-serial", type: "serial-list"},
+	{ text: "Digital Serial", value: "digital-serial", type: "serial-list"},
+	{ text: "Print Volume", value: "print-volume", type: "volume-list"},
+	{ text: "Digital Volume", value: "digital-volume", type: "volume-list"},
+	{ text: "Other", value: "other", type: "single-item"}
+];
