29ab83b3d7db55645bd0589a54b251fc403bdbf2
nicklaiacona <nick@performantsoftware.com>
2016-02-24 17:51:40 -0500

    start on validate name

--- app/assets/javascripts/dibb/models/bibliograph.js
+++ app/assets/javascripts/dibb/models/bibliograph.js
@@ -1,5 +1,14 @@
 /*global DiBB, Backbone */
-DiBB.Bibliograph = Backbone.Model.extend({});
+DiBB.Bibliograph = Backbone.Model.extend({
+  
+  validate: function(attributes, options) {
+        
+    if( attributes.name == null || attributes.name.length == 0 ) {
+      return { year: "Graph name cannot be blank." };
+    }    
+  }
+  
+});
 
 DiBB.BibliographCollection = Backbone.Collection.extend({
 
